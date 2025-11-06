b0737e99c83c9316a6206a077b491380fa96a939
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-06 18:03:14 +0100

    Feat : création de l'entité User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,83 +2,72 @@
 
 namespace App\Entity;
 
+use App\Repository\UserRepository;
 use App\Enum\UserRole;
+
 use App\Utils\RegexPatterns;
 
-use DateTimeImmutable;
 use InvalidArgumentException;
+use DateTimeImmutable;
 
-class User
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-    public function __construct(
-        public ?int $userId = null,
-        public ?string $email = null,
-        public ?string $password = null,
-        public ?UserRole $role = null,
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $email = null;
 
-        public ?DateTimeImmutable $createdAt = null,
-        public ?DateTimeImmutable $updatedAt = null,
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
 
-        public bool $isFromDatabase = false
-    ) {
-        $this
-            ->setUserEmail($email)
-            ->setUserRoles($role);
+    #[ORM\Column(type: 'string', length: 50, enumType: UserRole::class)]
+    private ?UserRole $role = null;
 
-        if ($this->isFromDatabase) {
-            $this->password = $password;
-        } else {
-            $this->setUserPassword($password);
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    private bool $isFromDatabase = false;
+
+    public function __construct()
+    {
+        if ($this->createdAt === null) {
+            $this->createdAt = new \DateTimeImmutable();
         }
 
-        $this->createdAt = $createdAt ?? new DateTimeImmutable();
-        $this->updatedAt = $updatedAt ?? new DateTimeImmutable();
+        if ($this->updatedAt === null) {
+            $this->updatedAt = $this->createdAt;
+        }
     }
 
-    // -------------Getters--------------
-
-    public function getUserId(): ?int
+    public function getId(): ?int
     {
-        return $this->userId;
+        return $this->id;
     }
 
-    public function getUserEmail(): ?string
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function getUserPassword(): ?string
+    public function setEmail(string $email): static
     {
-        return $this->password;
-    }
-
-    public function getUserRole(): ?UserRole
-    {
-        return $this->role;
-    }
-
-    public function getUserCreatedAt(): DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function getUserUpdatedAt(): DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    //------------Setter--------------
-
-    public function setUserId(?int $userId): self
-    {
-        $this->userId = $userId;
-        return $this;
-    }
-
-    public function setUserEmail(?string $email): self
-    {
-
         if ($email !== null) {
             $email = trim($email);
 
@@ -98,9 +87,13 @@ class User
         return $this;
     }
 
-    public function setUserPassword(?string $password, bool $isHashed = false): self
+    public function getPassword(): ?string
     {
+        return $this->password;
+    }
 
+    public function setPassword(string $password, bool $isHashed = false): static
+    {
         if ($password === null) {
             $this->password = null;
             return $this;
@@ -108,10 +101,10 @@ class User
 
         $passwordToStore = trim($password);
 
+
         if (!$isHashed) {
             $this->validatePassword($passwordToStore);
             $passwordToStore = password_hash($passwordToStore, PASSWORD_DEFAULT);
-            //var_dump('INSCRIPTION - HASH stocké:', $passwordToStore);
         }
 
         $this->password = $passwordToStore;
@@ -119,22 +112,44 @@ class User
         return $this;
     }
 
-    public function setHashedPasswordFromDb(?string $hashedPassword): self
+    public function getRole(): ?string
     {
-        // On assume que le mot de passe est déjà un HASH stocké.
-        $this->password = $hashedPassword;
-        return $this;
+        return $this->role;
     }
 
-    public function setUserRoles(?UserRole $role): self
+    public function getRoles(): array
+    {
+        $roles = ($this->role) ? [$this->role->value] : [];
+
+        // Assurez-vous qu'au moins 'ROLE_USER' est toujours présent
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRole(UserRole $role): static
     {
         $this->role = $role;
         $this->updateTimestamp();
         return $this;
     }
 
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
 
     // ----- Méthodes de vérification -----
+
+    // ne pas supprimer car fait partie de UserInterface
+    public function eraseCredentials(): void {}
 
     private function validatePassword(string $password): void
     {
