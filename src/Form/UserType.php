11ce14917db2d7ec250145e5c6da5d4f2f912c80
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-12 22:12:13 +0100

    Feat : finalisation

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,8 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Enum\UserRole;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -12,16 +16,29 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('role')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'exemple@domaine.com'
+                ],
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Mot de passe sécurisé'
+                ],
             ])
-        ;
+            /*
+            ->add('passwordConfirm', PasswordType::class, [
+                'label' => 'Confirmer le mot de passe',
+
+                'mapped' => false, // important : ne correspond pas à une propriété de l’entité
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Vérification du mot de passe',],
+            ])*/;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
