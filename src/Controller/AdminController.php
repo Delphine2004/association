beabe29cff0fa032ef0a82c7d406612e76e98a9f
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-12 22:12:43 +0100

    Feat : ajout du dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Form\UserSearchType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AdminController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard')]
-    public function index(): Response
+    public function adminAccount(Request $request, UserRepository $userRepository): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+
+        // Récupération des utilisateurs
+        $users = $userRepository->findAllUsers();
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
         ]);
     }
 }
