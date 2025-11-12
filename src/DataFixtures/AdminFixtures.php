f60ca50318df7bb37f9bd875ed82aa28b675c963
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-12 11:43:44 +0100

    Feat: ajout de l'admin

---
+++ src/DataFixtures/AdminFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Enum\UserRole;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AdminFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@association.fr');
+        $admin->setRole(UserRole::ADMIN);
+        $hashedPassword = $this->hasher->hashPassword($admin, 'admin123');
+        $admin->setPassword($hashedPassword, true);
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
