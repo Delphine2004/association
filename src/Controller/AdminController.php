db5e343781ffbb952969f7c4c07fb0bf257b0160
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-13 11:54:19 +0100

    Feat: ajout des routes admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Entity\User;
 use App\Enum\UserRole;
 
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+#[Route('/admin')]
 final class AdminController extends AbstractController
 {
-    #[Route('/admin/dashboard', name: 'admin_dashboard', methods: ['GET'])]
+    #[Route('/dashboard', name: 'admin_dashboard', methods: ['GET'])]
     public function adminAccount(UserRepository $userRepository): Response
     {
 
@@ -21,6 +26,90 @@ final class AdminController extends AbstractController
 
         return $this->render('admin/index.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('admin/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => false]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+
+            if (empty($plainPassword)) {
+                $this->addFlash('error', 'Le mot de passe est obligatoire.');
+                // Ici, on *ne redirige pas* : on laisse tomber la suite et on réaffiche le formulaire
+            } else {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Utilisateur créé avec succès.');
+                return $this->redirectToRoute('admin_dashboard');
+            }
+        }
+
+        // Que le formulaire soit invalide ou que le mdp soit vide, on revient ici pour afficher le formulaire
+        return $this->render('admin/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_dashboard', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => true]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Vérifie si le mot de passe a été modifié
+            $plainPassword = $form->get('password')->getData();
+
+            if (!empty($plainPassword)) {
+                $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur modifié avec succès.');
+            return $this->redirectToRoute('admin_dashboard', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 }
