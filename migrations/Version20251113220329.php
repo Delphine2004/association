de1532f8c216fd7742da0925bb706859e273a34f
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-14 00:13:43 +0100

    Nouvelle migration

--- migrations/Version20251113220329.php
+++ migrations/Version20251113220329.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251113140025 extends AbstractMigration
+final class Version20251113220329 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,27 +21,31 @@ final class Version20251113140025 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, description VARCHAR(255) NOT NULL, type VARCHAR(50) NOT NULL, race VARCHAR(50) NOT NULL, gender VARCHAR(50) NOT NULL, status VARCHAR(50) NOT NULL, vaccinated TINYINT(1) DEFAULT 0 NOT NULL, sterilized TINYINT(1) DEFAULT 0 NOT NULL, chipped TINYINT(1) DEFAULT 0 NOT NULL, compatible_kid TINYINT(1) DEFAULT 0 NOT NULL, compatible_cat TINYINT(1) DEFAULT 0 NOT NULL, compatible_dog TINYINT(1) DEFAULT 0 NOT NULL, birthday DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', arrival_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE animal_specification (animal_id INT NOT NULL, specification_id INT NOT NULL, INDEX IDX_24E22BBF8E962C16 (animal_id), INDEX IDX_24E22BBF908E2FFE (specification_id), PRIMARY KEY(animal_id, specification_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', place VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, animal_id INT DEFAULT NULL, url VARCHAR(255) NOT NULL, main TINYINT(1) DEFAULT 0 NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_16DB4F898E962C16 (animal_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE specification (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, category VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(50) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE animal_specification ADD CONSTRAINT FK_24E22BBF8E962C16 FOREIGN KEY (animal_id) REFERENCES animal (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE animal_specification ADD CONSTRAINT FK_24E22BBF908E2FFE FOREIGN KEY (specification_id) REFERENCES specification (id) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE user_animal (user_id INT NOT NULL, animal_id INT NOT NULL, INDEX IDX_FF93822A76ED395 (user_id), INDEX IDX_FF938228E962C16 (animal_id), PRIMARY KEY(user_id, animal_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_event (user_id INT NOT NULL, event_id INT NOT NULL, INDEX IDX_D96CF1FFA76ED395 (user_id), INDEX IDX_D96CF1FF71F7E88B (event_id), PRIMARY KEY(user_id, event_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F898E962C16 FOREIGN KEY (animal_id) REFERENCES animal (id)');
+        $this->addSql('ALTER TABLE user_animal ADD CONSTRAINT FK_FF93822A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_animal ADD CONSTRAINT FK_FF938228E962C16 FOREIGN KEY (animal_id) REFERENCES animal (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_event ADD CONSTRAINT FK_D96CF1FFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_event ADD CONSTRAINT FK_D96CF1FF71F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE animal_specification DROP FOREIGN KEY FK_24E22BBF8E962C16');
-        $this->addSql('ALTER TABLE animal_specification DROP FOREIGN KEY FK_24E22BBF908E2FFE');
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F898E962C16');
+        $this->addSql('ALTER TABLE user_animal DROP FOREIGN KEY FK_FF93822A76ED395');
+        $this->addSql('ALTER TABLE user_animal DROP FOREIGN KEY FK_FF938228E962C16');
+        $this->addSql('ALTER TABLE user_event DROP FOREIGN KEY FK_D96CF1FFA76ED395');
+        $this->addSql('ALTER TABLE user_event DROP FOREIGN KEY FK_D96CF1FF71F7E88B');
         $this->addSql('DROP TABLE animal');
-        $this->addSql('DROP TABLE animal_specification');
         $this->addSql('DROP TABLE event');
         $this->addSql('DROP TABLE picture');
-        $this->addSql('DROP TABLE specification');
         $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE user_animal');
+        $this->addSql('DROP TABLE user_event');
     }
 }
