1ee40932a3c04fc3f5c55c337b1f29244b93792e
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-14 18:42:58 +0100

    Feat : initialisation du formulaire

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,8 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Event;
+
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EventType extends AbstractType
@@ -12,16 +17,28 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date', null, [
-                'widget' => 'single_text',
+            ->add('date', DateType::class, [
+                'label' => 'Date de l\'événement',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => '01/06/2026'
+                ],
             ])
-            ->add('place')
-            ->add('description')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('place', TextType::class, [
+                'label' => 'Localisation de l\'événement',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'La forêt'
+                ],
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('description', TextareaType::class, [
+                'label' => 'Description de l\'événement',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 5,
+                    'placeholder' => 'L\'événement va se dérouler ...'
+                ],
             ])
         ;
     }
