0c70b2011ef7524edeaa5af850ae2df9503847ab
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-14 23:18:33 +0100

    Feat : initialisation

--- src/Form/AnimalType.php
+++ src/Form/AnimalType.php
@@ -3,9 +3,19 @@
 namespace App\Form;
 
 use App\Entity\Animal;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Enum\AnimalCategory;
+use App\Enum\AnimalRace;
+use App\Enum\AnimalGender;
+
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AnimalType extends AbstractType
@@ -13,25 +23,53 @@ class AnimalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('type')
-            ->add('race')
-            ->add('gender')
-            ->add('status')
-            ->add('vaccinated')
-            ->add('sterilized')
-            ->add('chipped')
-            ->add('compatibleKid')
-            ->add('compatibleCat')
-            ->add('compatibleDog')
-            ->add('birthday')
-            ->add('arrivalDate')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'animal',
+                'required' => true,
+                'attr' => ['class' => 'form-select',],
             ])
-            ->add('updatedAt', null, [
+            ->add('type', EnumType::class, [
+                'class' => AnimalCategory::class,
+                'label' => 'Type',
+                'choice_label' => 'name',
+                'placeholder' => 'Choisir un type',
+                'required' => true,
+                'attr' => ['class' => 'form-select',],
+            ])
+            ->add('race', EnumType::class, [
+                'class' => AnimalRace::class,
+                'label' => 'Race',
+                'choice_label' => 'name',
+                'placeholder' => 'Choisir une race',
+                'required' => true,
+                'attr' => ['class' => 'form-select',],
+            ])
+            ->add('gender', EnumType::class, [
+                'class' => AnimalGender::class,
+                'label' => 'Genre',
+                'choice_label' => 'name',
+                'placeholder' => 'Choisir un genre',
+                'required' => true,
+                'attr' => ['class' => 'form-select',],
+            ])
+            ->add('birthday', BirthdayType::class, [
+                'label' => 'Date de naissance',
+                'required' => false,
                 'widget' => 'single_text',
+                'html5' => true,
+                'attr' => ['class' => 'form-control',],
+            ])
+            ->add('arrivalDate', DateType::class, [
+                'label' => 'Date d\'arrivée',
+                'required' => true,
+                'widget' => 'single_text',
+                'html5' => true,
+                'attr' => ['class' => 'form-control',],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description de l\'animal',
+                'required' => false,
+                'attr' => ['class' => 'form-select',],
             ])
         ;
     }
