cb5f47dd4558d02ff09138b50ef88256c5c1e822
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-14 23:02:33 +0100

    Fix: correction de findFutureEvents

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Event;
 use DateTimeImmutable;
+use DateTimeInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -28,12 +29,35 @@ class EventRepository extends ServiceEntityRepository
 
     public function findFutureEvents(int $limit = 10, string $orderBy = 'ASC'): array
     {
-        $now = new DateTimeImmutable();
+        $now = new DateTimeImmutable('today');
 
         return $this->createQueryBuilder('e')
             ->where('e.date >= :now')
             ->setParameter('now', $now)
             ->orderBy('e.id',  $orderBy)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findEventsByFields(?array $criteria, int $limit = 10, string $orderBy = 'ASC'): array
+    {
+        $qb = $this->createQueryBuilder('e');
+
+        if (!empty($criteria['id'])) {
+            $qb->andWhere('e.id = :id')
+                ->setParameter('id', $criteria['id']);
+        }
+
+        if (!empty($criteria['date'])) {
+            $date = $criteria['date'];
+            if ($date instanceof \DateTimeInterface) {
+                $qb->andWhere('e.date = :date')
+                    ->setParameter('date', $date->format('Y-m-d'));
+            }
+        }
+
+        return $qb->orderBy('e.id',  $orderBy)
             ->setMaxResults($limit)
             ->getQuery()
             ->getResult();
