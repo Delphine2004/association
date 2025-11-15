8cf2728ed06ec7fd7d8bffdf4103c7bcedbf1746
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-15 16:21:06 +0100

    feat: ajout du staff

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Enum\UserRole;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail('staff@association.fr');
+        $admin->setRole(UserRole::EMPLOYE);
+        $hashedPassword = $this->hasher->hashPassword($admin, 'staff123');
+        $admin->setPassword($hashedPassword, true);
 
+        $manager->persist($admin);
         $manager->flush();
     }
 }
