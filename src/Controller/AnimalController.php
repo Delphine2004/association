71b4e8c6009007105288cb85b2b8c24967a245a6
Delphine FUMEX <delphinefumex@gmail.com>
2025-11-16 21:43:02 +0100

    Feat: finalisation des routes

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Animal;
-use App\Form\AnimalType;
 use App\Repository\AnimalRepository;
+use App\Entity\Animal;
+use App\Enum\AdoptionStatus;
+use App\Form\AnimalType;
+use App\Form\AnimalSearchStaffType;
+use App\Form\AnimalUpdateType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,31 +17,78 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/animal')]
 final class AnimalController extends AbstractController
 {
+
+    public function __construct(private string $uploadsAnimalsDirectory) {}
+
     #[Route(name: 'app_animal_index', methods: ['GET'])]
-    public function index(AnimalRepository $animalRepository): Response
-    {
+    public function index(
+        Request $request,
+        AnimalRepository $animalRepository
+    ): Response {
+
+        // Création du formulaire
+        $form = $this->createForm(AnimalSearchStaffType::class);
+        $form->handleRequest($request);
+
+        // Données du formulaire
+        $criteria = $form->getData();
+
+        $animals = $animalRepository->findAnimalsByFilters($criteria);
+
         return $this->render('animal/index.html.twig', [
-            'animals' => $animalRepository->findAll(),
+            'searchForm' => $form->createView(),
+            'animals' => $animals,
         ]);
     }
 
     #[Route('/new', name: 'app_animal_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
         $animal = new Animal();
         $form = $this->createForm(AnimalType::class, $animal);
         $form->handleRequest($request);
 
+        //dump($form->isSubmitted());
+        //dump($form->isValid());
+        //dump($form->getErrors(true));
+
         if ($form->isSubmitted() && $form->isValid()) {
+            //  Récupérer le fichier uploadé (UploadedFile ou null)
+            $uploadedFile = $form->get('picture')->getData();
+
+            if ($uploadedFile) {
+
+                //  Générer un nom unique (ex: 654a51b8d1e21.jpg)
+                $fileName = uniqid() . '.' . $uploadedFile->guessExtension();
+
+                //  Déplacer le fichier dans ton dossier uploads
+                $uploadedFile->move($this->uploadsAnimalsDirectory, $fileName);
+
+                //  Stocker le nom du fichier dans l'entité
+                $animal->setPicture($fileName);
+            }
+
+            $animal->setStatus(AdoptionStatus::EN_SOIN);
+            $animal->setVaccinated(false);
+            $animal->setSterilized(false);
+            $animal->setChipped(false);
+            $animal->setCompatibleKid(false);
+            $animal->setCompatibleCat(false);
+            $animal->setCompatibleDog(false);
+            $animal->setCreatedBy($this->getUser());
+
+
             $entityManager->persist($animal);
             $entityManager->flush();
 
+            $this->addFlash('success', 'Animal créé avec succès.');
             return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('animal/new.html.twig', [
-            'animal' => $animal,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -51,15 +101,30 @@ final class AnimalController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_animal_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Animal $animal, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(AnimalType::class, $animal);
+    public function edit(
+        Request $request,
+        Animal $animal,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $form = $this->createForm(AnimalUpdateType::class, $animal);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            // récupérations des caractéristiques santé
+            $isVaccinated = $form->get('vaccinated')->getData();
+            $isSterilized = $form->get('sterilized')->getData();
+            $isChipped = $form->get('chipped')->getData();
+
+            if ($isVaccinated && $isSterilized && $isChipped) {
+                $animal->setStatus(AdoptionStatus::A_ADOPTER);
+            }
+
+            $animal->setUpdatedBy($this->getUser());
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_animal_show', ['id' => $animal->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('animal/edit.html.twig', [
@@ -67,6 +132,28 @@ final class AnimalController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route('/{id}/adopt', name: 'app_animal_adopt', methods: ['POST'])]
+    public function adopt(
+        Request $request,
+        Animal $animal,
+        EntityManagerInterface $entityManager
+    ): Response {
+
+        if (!$this->isCsrfTokenValid('adopt' . $animal->getId(), $request->request->get('_token'))) {
+            throw $this->createAccessDeniedException('Token CSRF invalide.');
+        }
+
+        $animal->setStatus(AdoptionStatus::ADOPTE);
+        $animal->setUpdatedBy($this->getUser());
+
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Animal marqué comme adopté.');
+        return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
 
     #[Route('/{id}', name: 'app_animal_delete', methods: ['POST'])]
     public function delete(Request $request, Animal $animal, EntityManagerInterface $entityManager): Response
--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use App\Entity\Event;
 use App\Form\EventType;
-use App\Repository\EventRepository;
+use App\Form\EventSearchType;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,52 +16,94 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/event')]
 final class EventController extends AbstractController
 {
+
+    public function __construct(private string $uploadsEventsDirectory) {}
+
     #[Route(name: 'app_event_index', methods: ['GET'])]
-    public function index(EventRepository $eventRepository): Response
-    {
+    public function index(
+        Request $request,
+        EventRepository $eventRepository
+    ): Response {
+
+        // Création du formulaire
+        $form = $this->createForm(EventSearchType::class);
+        $form->handleRequest($request);
+
+        // Données du formulaire
+        $criteria = $form->getData();
+
+        $events = $eventRepository->findEventsByFields($criteria);
+
         return $this->render('event/index.html.twig', [
-            'events' => $eventRepository->findAll(),
+            'searchForm' => $form->createView(),
+            'events' => $events,
         ]);
     }
 
     #[Route('/new', name: 'app_event_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //  Récupérer le fichier uploadé (UploadedFile ou null)
+            $uploadedFile = $form->get('picture')->getData();
+
+            if ($uploadedFile) {
+
+                //  Générer un nom unique (ex: 654a51b8d1e21.jpg)
+                $fileName = uniqid() . '.' . $uploadedFile->guessExtension();
+
+                //  Déplacer le fichier dans ton dossier uploads
+                $uploadedFile->move($this->uploadsEventsDirectory, $fileName);
+
+                //  Stocker le nom du fichier dans l'entité
+                $event->setPicture($fileName);
+            }
+
+            $event->setCreatedBy($this->getUser());
+
             $entityManager->persist($event);
             $entityManager->flush();
 
+            $this->addFlash('success', 'Evénement créé avec succès.');
             return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('event/new.html.twig', [
-            'event' => $event,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_event_show', methods: ['GET'])]
-    public function show(Event $event): Response
-    {
+    public function show(
+        Event $event
+    ): Response {
         return $this->render('event/show.html.twig', [
             'event' => $event,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_event_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Event $event, EntityManagerInterface $entityManager): Response
-    {
+    public function edit(
+        Request $request,
+        Event $event,
+        EntityManagerInterface $entityManager
+    ): Response {
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $event->setUpdatedBy($this->getUser());
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_event_show', ['id' => $event->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('event/edit.html.twig', [
@@ -69,9 +113,12 @@ final class EventController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_event_delete', methods: ['POST'])]
-    public function delete(Request $request, Event $event, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->getPayload()->getString('_token'))) {
+    public function delete(
+        Request $request,
+        Event $event,
+        EntityManagerInterface $entityManager
+    ): Response {
+        if ($this->isCsrfTokenValid('delete' . $event->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($event);
             $entityManager->flush();
         }
