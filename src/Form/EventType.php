1f80ee609aa2124211c7fb88de39b893a5f23fca
Delphine FUMEX <dfumex2004@gmail.com>
2026-05-13 16:21:26 +0200

    Fix: nettoyage

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -7,7 +7,6 @@ use App\Entity\Event;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
